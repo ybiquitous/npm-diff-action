88ad60519bb617411384799876ca51fa6bc5b027
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-01-02 11:21:37 +0900

    test: migrate Jest to `node:test` (#378)

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from "@eslint/js"; // eslint-disable-line n/no-extraneous-import
 import { defineConfig } from "eslint/config";
 
 import nodePlugin from "eslint-plugin-n";
-import jestPlugin from "eslint-plugin-jest";
 
 export default defineConfig([
   {
@@ -17,10 +16,6 @@ export default defineConfig([
       ecmaVersion: "latest",
       sourceType: "module",
     },
-    extends: [
-      js.configs.recommended,
-      nodePlugin.configs["flat/recommended-module"],
-      jestPlugin.configs["flat/style"],
-    ],
+    extends: [js.configs.recommended, nodePlugin.configs["flat/recommended-module"]],
   },
 ]);
